cfba2bd70b2b2d01cd6330b9606eb0ade94ee570
korpon <108818506+korpon@users.noreply.github.com>
2023-06-07 16:32:07 -0400

    PR for Sprint 003 (#5)

--- stuffs.py
+++ stuffs.py
@@ -1,3 +1,16 @@
+import os
+
 DATA_FOLDER = "C:/stellar-grove/tara/data/"
 DATA_FOLDER_SONGROVE = "C:/stellar-grove/tara/SongroveBotanicals/data/"
+
+
+# ------ Begin Constants ----- #
+peep = os.environ["USERNAME"]
+homeD = f'{os.environ["HOMEDRIVE"]}{os.environ["HOMEPATH"]}'.replace('\\','/')
+
+SGWD = f'{homeD}/Stellar Grove/'
+bitsWD = f'{SGWD}bitstaemr - Documents/'
+taraWD = f'{SGWD}ticondagrova - Documents/'
+
+kaggleWD = f'{taraWD}Kaggle/'
 
