7b932b52467142f25e48065038c4c5bbf98c33db
Funk Doctor Spock <dk@stellargrove.com>
2024-05-01 14:31:07 -0400

    updating TSP to reflect it as a class

--- distributions.py
+++ distributions.py
@@ -4,136 +4,140 @@ import random
 import pandas as pd
 import sys; sys.path.append("../")
 
-def TSP(LowBound, Middle , UpperBound, n):
-    '''
-    This functions provides the details of a given TSP distribution.  For example, you provide the parameters and it 
-    will return: E(x), Var, alpha, beta, p & q.  These parameters are calculated using van Dorps paper. 
-
-    params: LowBound, Middle, UpperBound, n
-
-    results: the values associated with the TSP, mean, variance, etc.
-    
-    '''
-    # Set any upfront variables
-    params = [LowBound, Middle, UpperBound]
-    weights = [float(1/6), float(4/6), float(1/6)]
-    #value1 = (2-2^(0.5))/4
-    # Calculate values
-    expected_value = sum_product(np.array(params), np.array(weights))
-    variance = ((UpperBound - LowBound)**2) / 36
-    alpha = (expected_value - LowBound) / (UpperBound - LowBound)
-    alpha2 = variance / (UpperBound - LowBound)**2
-    beta = ((alpha*(1-alpha)) / alpha2) - 1
-    p = alpha * beta
-    q = (1 - alpha) * beta
-    # Write to the payload dictionary
-    payload = {}
-    payload["expected_value"] = expected_value
-    payload["variance"] = variance
-    payload["alpha"] = alpha
-    payload["beta"] = beta
-    payload["p"] = p
-    payload["q"] = q
-    return payload
-
-# parameters list takes the values [Low, Mid, Hi, n]
-def createTSPSample(parameterList:list, size):
-    """
-    This function creates a sample of data that is distributed according to the TSP that has a list of
-    parameters passed in the function.
-
-    params: Low, Middle, High, n
-    return: sample set of data distributed according to TSP(L,M,H,n)
-    """
-
-    listSample = np.random.uniform(0, 1, size)
-    listValues = [generateTSP(parameterList, sample) for sample in listSample]
-    listCombined = [listSample, listValues]
-    dfSample = pd.DataFrame(listCombined).T
-    dfSample.columns = ["randomSampleValue", "GeneratedTSPValue"]
-    return dfSample
-
-def generateTSP(parametersList: list, sample):
-    """
-    This function returns a singular value of a TSP distribution according to the 
-    parameters list.  For it example it will generate a number like 14, given a value
-    from 0 -1, i.e. p.
-
-    params: parameterList -> [low, middle, hi, n], sample -> value ranging from 0 - 1
-    return: value between low and high
-    """
-    LowBound = float(parametersList[0])
-    Mid = float(parametersList[1])
-    HighBound = float(parametersList[2])
-    n = parametersList[3]
-    x_value = sample
-    FM = (Mid - LowBound) / (HighBound - LowBound)
-    BoundRange = HighBound - LowBound
-    lower_value = pow(x_value * BoundRange * pow(Mid - LowBound, n-1),(1/n))
-    upper_value = pow((1 - x_value) * BoundRange * pow(HighBound - Mid, n-1),(1/n))
-    FYL = LowBound + lower_value
-    FYU = HighBound - upper_value
-    if x_value < FM: 
-        generated_value = FYL
-    else: generated_value = FYU
-    return generated_value
-
-def checkTSPParams(parametersList: list):
-    """
-    This is a helper function to ensure that the correct number of parameters is being passed.
-
-    params: parametersList
-    return: error message "len not correct" on errors.
-    """
-    if len(parametersList) != 4: return "length not correct"
-
 def sum_product(a,b):
 
     return np.sum(a * b)
 
-def getDefaultParameters():
-        """
-        This routine provides a dictionary with all the default parameters for all distributions contained in this 
-        module.
-        """
-        dict_config = {
-            "sampleSize":100,
-            "tsp":{
-                    "name":"tsp",
-                    "low":13,
-                    "mid":18,
-                    "hi":25,
-                    "n":2
-                    },
-            "normal":{
-                    "name":"normal",
-                    "mean":3,
-                    "std":1.4
-                   },
-            "poisson":{
-                        "name":"poisson",
-                        "mu":4
-                    },
-            "binomial":{
-                        "name":"binomial",
-                        "n":5,
-                        "p":0.4
-                        },
-            "bernoulli":{
-                        "name":"bernoulli",
-                        "p":0.271
-                        }
-                
-        }
-        return dict_config
 
-def getDataFrameNames():
-    """
-    This function provides the different types of names 
-    that a data frame could be listed to.
-    """
-    lst = ["dataframe", "df", "data-frame"]
-    return lst
+class TwoSidedPower(object):
+
+    def moments(self, LowBound, Middle , UpperBound, n):
+        '''
+        This functions provides the details of a given TSP distribution.  For example, you provide the parameters and it 
+        will return: E(x), Var, alpha, beta, p & q.  These parameters are calculated using van Dorps paper. 
+
+        params: LowBound, Middle, UpperBound, n
+
+        results: the values associated with the TSP, mean, variance, etc.
+        
+        '''
+        # Set any upfront variables
+        params = [LowBound, Middle, UpperBound]
+        weights = [float(1/6), float(4/6), float(1/6)]
+        #value1 = (2-2^(0.5))/4
+        # Calculate values
+        expected_value = sum_product(np.array(params), np.array(weights))
+        variance = ((UpperBound - LowBound)**2) / 36
+        alpha = (expected_value - LowBound) / (UpperBound - LowBound)
+        alpha2 = variance / (UpperBound - LowBound)**2
+        beta = ((alpha*(1-alpha)) / alpha2) - 1
+        p = alpha * beta
+        q = (1 - alpha) * beta
+        # Write to the payload dictionary
+        payload = {}
+        payload["expected_value"] = expected_value
+        payload["variance"] = variance
+        payload["alpha"] = alpha
+        payload["beta"] = beta
+        payload["p"] = p
+        payload["q"] = q
+        return payload
+
+    # parameters list takes the values [Low, Mid, Hi, n]
+    def createTSPSample(self, parameterList:list, size):
+        """
+        This function creates a sample of data that is distributed according to the TSP that has a list of
+        parameters passed in the function.
+
+        params: Low, Middle, High, n
+        return: sample set of data distributed according to TSP(L,M,H,n)
+        """
+
+        listSample = np.random.uniform(0, 1, size)
+        listValues = [self.moments(parameterList, sample) for sample in listSample]
+        listCombined = [listSample, listValues]
+        dfSample = pd.DataFrame(listCombined).T
+        dfSample.columns = ["randomSampleValue", "GeneratedTSPValue"]
+        return dfSample
+
+    def generateTSP(parametersList: list, sample):
+        """
+        This function returns a singular value of a TSP distribution according to the 
+        parameters list.  For it example it will generate a number like 14, given a value
+        from 0 -1, i.e. p.
+
+        params: parameterList -> [low, middle, hi, n], sample -> value ranging from 0 - 1
+        return: value between low and high
+        """
+        LowBound = float(parametersList[0])
+        Mid = float(parametersList[1])
+        HighBound = float(parametersList[2])
+        n = parametersList[3]
+        x_value = sample
+        FM = (Mid - LowBound) / (HighBound - LowBound)
+        BoundRange = HighBound - LowBound
+        lower_value = pow(x_value * BoundRange * pow(Mid - LowBound, n-1),(1/n))
+        upper_value = pow((1 - x_value) * BoundRange * pow(HighBound - Mid, n-1),(1/n))
+        FYL = LowBound + lower_value
+        FYU = HighBound - upper_value
+        if x_value < FM: 
+            generated_value = FYL
+        else: generated_value = FYU
+        return generated_value
+
+    def checkTSPParams(parametersList: list):
+        """
+        This is a helper function to ensure that the correct number of parameters is being passed.
+
+        params: parametersList
+        return: error message "len not correct" on errors.
+        """
+        if len(parametersList) != 4: return "length not correct"
+
+ 
+    def getDefaultParameters():
+            """
+            This routine provides a dictionary with all the default parameters for all distributions contained in this 
+            module.
+            """
+            dict_config = {
+                "sampleSize":100,
+                "tsp":{
+                        "name":"tsp",
+                        "low":13,
+                        "mid":18,
+                        "hi":25,
+                        "n":2
+                        },
+                "normal":{
+                        "name":"normal",
+                        "mean":3,
+                        "std":1.4
+                    },
+                "poisson":{
+                            "name":"poisson",
+                            "mu":4
+                        },
+                "binomial":{
+                            "name":"binomial",
+                            "n":5,
+                            "p":0.4
+                            },
+                "bernoulli":{
+                            "name":"bernoulli",
+                            "p":0.271
+                            }
+                    
+            }
+            return dict_config
+
+    def getDataFrameNames():
+        """
+        This function provides the different types of names 
+        that a data frame could be listed to.
+        """
+        lst = ["dataframe", "df", "data-frame"]
+        return lst
 
 class DaCountDeMonteCarlo(object):
     """
