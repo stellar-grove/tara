ab117f0a6428f20ce793b3ec02fab22a275cd773
Funk Doctor Spock <dk@stellargrove.com>
2024-06-10 15:59:58 -0400

    updating unit test files

--- tests/test_ticondagrova.py
+++ tests/test_ticondagrova.py
@@ -19,7 +19,7 @@ class TestDistributions:
         m = 18
         h = 23
         n = 2
-        dict_TSP = dists.TSP(l,m,h,n)
+        dict_TSP = dists.TwoSidedPower(l,m,h,n)
         dict_test = tara_test.TEST_TSP
         check_dictionary_keys(dict_TSP, dict_test)
         check_dictionary_values(dict_TSP, dict_test)
@@ -28,7 +28,7 @@ class TestDistributions:
     def test_generateTSP(self):
         sample = 0.3305
         lst_params = [10, 18, 23,2]
-        tsp_sample = dists.generateTSP(lst_params,sample)
+        tsp_sample = dists.TwoSidedPower.generateTSP(lst_params,sample)
         tsp_expected = tara_test.TSP_SAMPLE_EXPECTED
         assert round(tsp_sample,4) == round(tsp_expected,4)
         
